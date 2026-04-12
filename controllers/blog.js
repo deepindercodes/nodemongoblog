540f0a57b9fdc22140d81de040f2673d1ab9a44d
Deepinder Singh <deepindersingh006@gmail.com>
2026-04-13 00:29:35 +0530

    Adding navigation bar, login and signup page HTMLs

--- controllers/blog.js
+++ controllers/blog.js
@@ -1,5 +1,20 @@
 exports.getIndexPage = (req,res,next) =>{
     res.render("blog/index" ,{
-        pageTitle:"Welcome to NodeMongoBlog"
+        pageTitle:"Welcome to NodeMongoBlog",
+        path: "/"
+    });
+}
+
+exports.getLogin = (req, res, next) => {
+    res.render("blog/login" ,{
+        pageTitle:"Login to NodeMongoBlog",
+        path: "/login"
+    });
+}
+
+exports.getSignup = (req, res, next) => {
+    res.render("blog/signup" ,{
+        pageTitle:"Signup to NodeMongoBlog",
+        path: "/signup"
     });
 }
--- routes/blog.js
+++ routes/blog.js
@@ -6,5 +6,12 @@ const blogRoutes = new Router();
 //Index page of the website
 blogRoutes.get("/",blogController.getIndexPage);
 
+//Login page GET route
+blogRoutes.get("/login", blogController.getLogin);
+
+
+//SignUp page GET route
+blogRoutes.get("/signup", blogController.getSignup);
+
 
 module.exports = blogRoutes;
